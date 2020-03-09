0d5dab1bf47c56575c82715d6e65ca19d0103c89
somark2000 <61942320+somark2000@users.noreply.github.com>
2020-03-09 19:39:54 +0200

    Add files via upload

--- complex.h
+++ complex.h
@@ -1,1 +1,31 @@
 #pragma once
+using namespace std;
+
+class Complex
+{
+private:
+	double real, imaginar;
+public:
+	//constructor
+	Complex();
+
+	//return imaginar
+	double show_imaginar();
+
+	//return real
+	double show_real();
+
+	//add
+	Complex operator+ (Complex z);
+
+	//mul
+	Complex operator*  (Complex z);
+
+	//quot
+	Complex operator/ (Complex z);
+
+
+
+	//destructor
+	~Complex();
+};
